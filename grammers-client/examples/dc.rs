b7e50b1a580d7fba4ec7f91193d17cb3b53b165a
it00021hot <liu15077731547@gmail.com>
2025-12-12 10:09:16 +0800

    dc test

--- grammers-client/examples/dc.rs
+++ grammers-client/examples/dc.rs
@@ -13,20 +13,20 @@
 #![allow(deprecated)]
 
 use grammers_client::Client;
-use grammers_mtsender::{InvocationError, SenderPool};
+use grammers_mtsender::SenderPool;
 use grammers_session::Session;
 use grammers_session::storages::MemorySession;
-use grammers_session::types;
+use grammers_session::types::DcOption;
 use grammers_tl_types::enums::help::CountriesList;
-use grammers_tl_types::functions::auth::SendCode;
 use grammers_tl_types::functions::help::GetCountriesList;
-use grammers_tl_types::types::CodeSettings;
 use log::info;
 use simple_logger::SimpleLogger;
 use std::net::{Ipv4Addr, SocketAddrV4, SocketAddrV6};
 use std::sync::Arc;
 use tokio::runtime;
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+const API_ID: i32 = 2040;
 
 const fn ipv4(a: u8, b: u8, c: u8, d: u8) -> SocketAddrV4 {
     SocketAddrV4::new(Ipv4Addr::new(a, b, c, d), 10443)
@@ -38,11 +38,22 @@ const fn ipv6(a: u8, b: u8, c: u8, d: u8) -> SocketAddrV6 {
 
 /// 测试服DC配置
 
-pub(crate) const KNOWN_DC_OPTIONS: [types::DcOption; 1] = [
-    types::DcOption {
+pub(crate) const KNOWN_DC_OPTIONS: [DcOption; 2] = [
+    DcOption {
         id: 1,
-        ipv4: ipv4(127,0,0,1),
-        ipv6: ipv6(127,0,0,1),
+        ipv4: ipv4(127, 0, 0, 1),
+        ipv6: ipv6(127, 0, 0, 1),
+        auth_key: None,
+    },
+    DcOption {
+        id: 2,
+        ipv4: SocketAddrV4::new(Ipv4Addr::new(47, 237, 112, 224), 35349),
+        ipv6: SocketAddrV6::new(
+            ipv4(47, 237, 112, 224).ip().to_ipv6_compatible(),
+            35349,
+            0,
+            0,
+        ),
         auth_key: None,
     },
 ];
@@ -52,9 +63,12 @@ async fn async_main() -> Result<()> {
         .with_level(log::LevelFilter::Debug)
         .init()
         .unwrap();
+    send_to_one().await?;
+    send_to_two().await?;
+    Ok(())
+}
 
-    let api_id = 2040;
-    let api_hash = "b18441a1ff607e10a989891a5462e627";
+async fn send_to_one() -> Result<()> {
     let s = MemorySession::default();
     for dc_option in KNOWN_DC_OPTIONS.iter() {
         s.set_dc_option(dc_option);
@@ -62,36 +76,35 @@ async fn async_main() -> Result<()> {
     s.set_home_dc_id(1);
     let session = Arc::new(s);
 
-    let pool = SenderPool::new(Arc::clone(&session), api_id);
+    let pool = SenderPool::new(Arc::clone(&session), API_ID);
     let client = Client::new(&pool);
     let SenderPool { runner, .. } = pool;
     let _ = tokio::spawn(runner.run());
-
-    let phone = "6807793198"; // random phone
-    let request = SendCode {
-        phone_number: phone.to_string(),
-        api_id,
-        api_hash: api_hash.to_string(),
-        settings: CodeSettings {
-            allow_flashcall: false,
-            current_number: false,
-            allow_app_hash: false,
-            allow_missed_call: false,
-            allow_firebase: false,
-            logout_tokens: None,
-            token: None,
-            app_sandbox: None,
-            unknown_number: false,
-        }
-            .into(),
+    let req = GetCountriesList {
+        lang_code: "en".parse().unwrap(),
+        hash: 0,
     };
-
-    let dc = match client.invoke(&request).await {
-        Ok(_) => session.home_dc_id(),
-        Err(InvocationError::Rpc(err)) if err.code == 303 => err.value.unwrap() as i32,
+    let countries = match client.invoke(&req).await {
+        Ok(resp) => resp.clone(),
         Err(e) => return Err(e.into()),
     };
-    info!("DC: {}", dc);
+    match countries {
+        CountriesList::NotModified => {}
+        CountriesList::List(list) => list.countries.iter().for_each(|country| {
+            info!("Country: {:?}", country);
+        }),
+    }
+    Ok(())
+}
+
+async fn send_to_two() -> Result<()> {
+    let s = MemorySession::default();
+    let session = Arc::new(s);
+
+    let pool = SenderPool::new(Arc::clone(&session), API_ID);
+    let client = Client::new(&pool);
+    let SenderPool { runner, .. } = pool;
+    let _ = tokio::spawn(runner.run());
     let req = GetCountriesList {
         lang_code: "en".parse().unwrap(),
         hash: 0,
